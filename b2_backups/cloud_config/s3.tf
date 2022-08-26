746acc4d3c17052978eb97f2b9a23f6671426b7d
Vladislav Yaroshchuk <Vladislav.Yaroshchuk@jetbrains.com>
2022-08-26 13:50:01 +0300

    [b2] Keep only last version of file in b2

--- b2_backups/cloud_config/s3.tf
+++ b2_backups/cloud_config/s3.tf
@@ -1,4 +1,10 @@
 resource "b2_bucket" "parents_nas_nextcloud" {
   bucket_name = "parents-nas-nextcloud"
   bucket_type = "allPrivate"
+
+  lifecycle_rules {
+    file_name_prefix = ""
+    days_from_hiding_to_deleting  = 1
+    days_from_uploading_to_hiding = 0
+  }
 }
