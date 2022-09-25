646617f9ec411abe491ab0b45a8f0f1e50acd247
Vladislav Yaroshchuk <Vladislav.Yaroshchuk@jetbrains.com>
2022-09-25 21:18:20 +0400

    [b2] Add duplicity configs

---
+++ b2_backups/duplicity/duplicity-backup.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+set -euxo pipefail
+
+source "$HOME/.local/config/duplicity/duplicity-settings.sh"
+
+: "$GPG_KEY_ID"
+
+: "$BACKUP_ROOT"
+
+: "$B2_KEY_ID"
+: "$B2_APPLICATION_KEY"
+: "$B2_BUCKET"
+: "$S3_ENDPOINT_URL"
+
+export AWS_ACCESS_KEY_ID="${B2_KEY_ID}"
+export AWS_SECRET_ACCESS_KEY="${B2_APPLICATION_KEY}"
+S3_URL="boto3+s3://${B2_BUCKET}"
+
+backup() {
+    duplicity \
+        --encrypt-key "${GPG_KEY_ID}" \
+        --use-agent \
+        --asynchronous-upload \
+        --verbosity 8 \
+        --s3-endpoint-url "${S3_ENDPOINT_URL}" \
+        "${BACKUP_ROOT}" \
+        "${S3_URL}"
+
+    duplicity \
+        cleanup \
+        --use-agent \
+        --force \
+        --s3-endpoint-url "${S3_ENDPOINT_URL}" \
+        "${S3_URL}"
+}
+
+list() {
+    duplicity \
+        list-current-files \
+        --use-agent \
+        --s3-endpoint-url "${S3_ENDPOINT_URL}" \
+        "${S3_URL}"
+}
+
+verify() {
+    duplicity \
+        verify \
+        --use-agent \
+        --compare-data \
+        --file-to-restore "$1" \
+        --s3-endpoint-url "${S3_ENDPOINT_URL}" \
+        "${S3_URL}" \
+        "${BACKUP_ROOT}/$1"
+}
+
+restore-all() {
+    duplicity \
+        --use-agent \
+        --s3-endpoint-url "${S3_ENDPOINT_URL}" \
+        "${S3_URL}" \
+        "$1"
+}
+
+"$@"
---
+++ b2_backups/duplicity/install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -euxo pipefail
+
+: "$B2_KEY_ID"
+: "$B2_APPLICATION_KEY"
+
+gpg import key.pub
+
+mkdir -p "$HOME/.config/systemd/user"
+mkdir -p "$HOME/.local/bin"
+mkdir -p "$HOME/.local/config/duplicity"
+
+envsubst < "duplicity-settings.sh.template" > "duplicity-settings.sh"
+
+install -Dm700 "duplicity-backup.sh" "$HOME/.local/bin/"
+install -Dm700 "duplicity-settings.sh" "$HOME/.local/config/duplicity/"
+
+install -Dm644 "systemd/duplicity-backup.service" "$HOME/.config/systemd/user/"
+install -Dm644 "systemd/duplicity-backup.timer" "$HOME/.config/systemd/user/"
+systemctl --user enable --now duplicity-backup.timer
