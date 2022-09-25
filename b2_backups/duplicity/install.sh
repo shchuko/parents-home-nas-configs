8dfdeb8d3c4288181b775bfbfa3cb3e71168260f
Vladislav Yaroshchuk <Vladislav.Yaroshchuk@jetbrains.com>
2022-09-25 21:59:17 +0400

    [b2/duplicity] Use systemd EnvironmentFile property

--- b2_backups/duplicity/install.sh
+++ b2_backups/duplicity/install.sh
@@ -7,16 +7,19 @@ set +x
 set -x
 
 gpg --import key.pub
+pip3 install boto3
 
 mkdir -p "$HOME/.config/systemd/user"
 mkdir -p "$HOME/.local/bin"
 mkdir -p "$HOME/.local/config/duplicity"
 
-envsubst < "duplicity-settings.sh.template" > "duplicity-settings.sh"
+envsubst < "duplicity-settings.env.template" > "duplicity-settings.env"
 
 install -Dm700 "duplicity-backup.sh" "$HOME/.local/bin/"
-install -Dm700 "duplicity-settings.sh" "$HOME/.local/config/duplicity/"
+install -Dm700 "duplicity-settings.env" "$HOME/.local/config/duplicity/"
 
 install -Dm644 "systemd/duplicity-backup.service" "$HOME/.config/systemd/user/"
 install -Dm644 "systemd/duplicity-backup.timer" "$HOME/.config/systemd/user/"
+
+systemctl --user daemon-reload
 systemctl --user enable --now duplicity-backup.timer
